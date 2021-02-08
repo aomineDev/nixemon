3ee0ee9b6a795046bfdede86a9e44b87aa3bc91f
aomine <aomine1745@gmail.com>
2021-02-07 22:34:11 -0500

    remove logs

--- src/utils/findCommandAlias.ts
+++ src/utils/findCommandAlias.ts
@@ -3,12 +3,8 @@ import Command from '../interfaces/Command'
 
 export default function findCommandAlias (commandName: string, commands: Commands): Command | undefined {
   let command: Command | undefined
-  console.log('commandName', commandName)
 
   for (const cmd of commands.values()) {
-    console.log('command name', cmd.name)
-    console.log('incldues commandName?', cmd.aliases?.includes(commandName))
-    console.log('----------------------------------')
     if (cmd.aliases?.includes(commandName) === true) {
       command = cmd
       break
